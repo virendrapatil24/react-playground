8493d4a14008929da7b755fd87d54f443a6a7797
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-06-09 17:24:22 +0530

    added some sharing state logic between components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Button from "./components/Button/Button";
 import { useState } from "react";
 import { IoCalendarClear } from "react-icons/io5";
 import Like from "./components/Like";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
   let items = [
@@ -21,6 +23,8 @@ function App() {
 
   const [alertVisible, setAlertVisibility] = useState(false);
 
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+
   return (
     <>
       {/* <ListGroup
@@ -33,8 +37,10 @@ function App() {
       )}
       <Button color="secondary" onClick={() => setAlertVisibility(true)}>
         My Button
-      </Button> */}
-      <Like onClick={() => console.log("You clicked the Like button!")} />
+      </Button>
+      <Like onClick={() => console.log("You clicked the Like button!")} /> */}
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClearCart={() => setCartItems([])} />
     </>
   );
 }
